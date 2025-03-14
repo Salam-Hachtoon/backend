4740f75b28e2e646569ffb07c2dd892bbc5dba6d
hima890 <hfibrahim90@gmail.com>
2025-03-14 23:13:26 +0200

    feat: Add Celery configuration for asynchronous task management

---
+++ api/api/celery.py
@@ -0,0 +1,23 @@
+"""
+Celery configuration file for Django API project.
+"""
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog_API.settings')
+
+# Create celery app instance
+app = Celery('api')
+
+# Load celery config from Django settings
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Set the autodiscover to True
+app.autodiscover_tasks()
+
+# Import periodic tasks for Celery Beat
+from celery.schedules import crontab
+from celery import shared_task
