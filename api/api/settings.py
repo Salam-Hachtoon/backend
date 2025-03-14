ef6be6c56bb43196464d9884585367a4afae0bd5
hima890 <hfibrahim90@gmail.com>
2025-03-15 00:49:28 +0200

    feat: Add media settings for handling uploaded files

--- api/api/settings.py
+++ api/api/settings.py
@@ -199,6 +199,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media Settings
+MEDIA_URL = "/media/"  # Public URL for accessing media files
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # Directory where uploaded files will be stored
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
