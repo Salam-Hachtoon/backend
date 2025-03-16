acb3a9d3e78552761ab8c095c447961d0cf895e6
hima890 <hfibrahim90@gmail.com>
2025-03-16 12:57:24 +0200

    feat: Add send_email_task to celery tasks for sending emails with attachments

--- api/users/celery_tasks.py
+++ api/users/celery_tasks.py
@@ -1,10 +1,13 @@
 import logging
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken # type: ignore
 from django.utils.timezone import now
 from celery import shared_task
+from .utility import send_email_with_attachments
+
 
 # Create the looger instance for the celery tasks
 logger = logging.getLogger('celery_tasks')
+
 
 @shared_task
 def clean_expired_blacklisted_tokens():
@@ -25,3 +28,20 @@ def clean_expired_blacklisted_tokens():
         logger.info("Deleted {} expired blacklisted tokens.".format(count))
     except Exception as e:
         logger.error("Failed to delete expired blacklisted tokens: {}".format(e))
+
+
+@shared_task
+def send_email_task(subject, template_name, context, recipient_list, attachments=None):
+    """
+    Sends an email with the specified subject, template, context, and recipient list, optionally including attachments.
+    Args:
+        subject (str): The subject of the email.
+        template_name (str): The name of the email template to use.
+        context (dict): A dictionary of context variables to render the template.
+        recipient_list (list): A list of recipient email addresses.
+        attachments (list, optional): A list of file attachments to include in the email. Defaults to None.
+    Returns:
+        bool: True if the email was sent successfully, False otherwise.
+    """
+    
+    return send_email_with_attachments(subject, template_name, context, recipient_list, attachments)
