a73cdb4453d9f5a7cb4e1a889873833908a2f36c
hima890 <hfibrahim90@gmail.com>
2025-03-19 15:06:26 +0200

    feat: Trigger get_text_task on post_save event of Attachment model

--- api/ai_assistant/aiSignal.py
+++ api/ai_assistant/aiSignal.py
@@ -2,8 +2,8 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Attachment
-
+from .celery_tasks import get_text_task
 
 @receiver(post_save, sender=Attachment)
 def get_text():
-    pass
+    get_text_task.delay()
