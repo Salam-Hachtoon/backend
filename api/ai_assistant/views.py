5a589b2f2f090c8578bf08245dc6139fc8dbd7ff
hima890 <hfibrahim90@gmail.com>
2025-03-19 13:26:01 +0200

    feat: Add upload_attachments endpoint with authentication to handle file uploads

--- api/ai_assistant/views.py
+++ api/ai_assistant/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework.permissions import AllowAny, IsAuthenticated # type: ignore
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def upload_attachments(request):
+    pass
