2b298f90eea59a0b0864a292648e8fa3119b74e6
hima890 <hfibrahim90@gmail.com>
2025-03-20 19:14:12 +0200

    feat: Add delete_bookmark endpoint to the API for removing user bookmarks

--- api/ai_assistant/urls.py
+++ api/ai_assistant/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('get_flash_cards/',  views.get_flash_cards, name='get_flash_cards'),
     path('get_quiz/', views.get_quiz, name='get_quiz'),
     path('create_bookmark/', views.create_bookmark, name='create_bookmark'),
+    path('delete_bookmark/', views.delete_bookmark, name='delete_bookmark'),
 ]
